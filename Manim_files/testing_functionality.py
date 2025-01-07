1c9110994d6b1962ff9158ab4a9f545f38401645
Jan Torres Silvestre <github@j.xin.cat>
2025-01-07 20:15:49 +0100

    Deleted testing_funcitonality

--- Manim_files/testing_functionality.py
+++
@@ -1,6 +0,0 @@
-from manimanims.extra_code.graph_functions import myGraph
-
-vertices = [i + 1 for i in range(6)]
-edges = [[(i+1)%5+1] for i in range(6)]
-print(edges)
-g = myGraph(vertices=vertices, edges=edges)
